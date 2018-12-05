83d7604ad228b8b74d786a81efd4ba419e9c3c78
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-12-05 23:19:45 +0100

    JSON Web Token Signing and Validation Logic

--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import(
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	jwt "github.com/dgrijalva/jwt-go"
 	log "github.com/sirupsen/logrus"
+	"github.com/mitchellh/mapstructure"
+	"github.com/gorilla/mux"
 )
 
 type User struct {
@@ -22,15 +24,38 @@ type Exception struct {
     Message string `json:"message"`
 }
 
-func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request) {}
+func CreateTokenEndpoint(w http.ResponseWriter, req *http.Request) {
+	user := User{}
+	if err := json.NewDecoder(req.Body).Decode(&user) ; err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": user.Username,
+			"password": user.Password,
+	})
+	tokenString, error := token.SignedString([]byte("secret"))
+	if error != nil {
+			fmt.Println(error)
+	}
+	json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
+}
 
 func ProtectedEndpoint(w http.ResponseWriter, req *http.Request) {
-	j, err := json.Marshal(Exception{ Message: "not implement yet" })
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal Server Error"))
+	params := req.URL.Query()
+	token, _ := jwt.Parse(params["token"][0], func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("There was an error")
+			}
+			return []byte("secret"), nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			user := User{}
+			mapstructure.Decode(claims, &user)
+			json.NewEncoder(w).Encode(user)
+	} else {
+			json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
 	}
-	w.Write(j)
 }
 
 func main() {
